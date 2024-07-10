76d09bc8d786abda4b567b3624dbbfe81d5184e4
Vince Vizza <vinnyv1103@gmail.com>
2024-07-09 22:25:25 -0500

    Import App.css and App.jsx and TODO.md from my source code

---
+++ TODO.md
@@ -0,0 +1,2 @@
+check if I should add noscript tag to mail html above <div id="root"> for incase they user has JavaScript disabled.
+    <noscript>You need to enable JavaScript to run this app.</noscript>
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import CoffeePage from './components/CoffeePage.jsx'
+import CoffeeInputForm from './components/CoffeeInputForm.jsx'
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(null)
 
+  function setPage() {
+    switch (currentPage) {
+      case "coffee": return <CoffeePage setPage={setCurrentPage} />
+      case "coffee-input": return <CoffeeInputForm setPage={setCurrentPage} />
+      // case "tea": return <TeaPage setPage={setCurrentPage} />
+      default: return <Home setPage={setCurrentPage} />
+    }
+
+  }
   return (
-    <>
-     
-    </>
+    <div className="App">
+      {setPage()}
+    </div>
   )
 }
 
